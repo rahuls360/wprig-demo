372c7815cc503542eb347bdfb4331f71b8e10287
Rahul Makhija <rahuls360@gmail.com>
2021-01-13 16:06:41 +0530

    initial commit

---
+++ themes/wprig-customtheme/functions.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * WP Rig functions and definitions
+ *
+ * This file must be parseable by PHP 5.2.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package wp_rig
+ */
+
+define( 'WP_RIG_MINIMUM_WP_VERSION', '4.5' );
+define( 'WP_RIG_MINIMUM_PHP_VERSION', '7.0' );
+
+// Bail if requirements are not met.
+if ( version_compare( $GLOBALS['wp_version'], WP_RIG_MINIMUM_WP_VERSION, '<' ) || version_compare( phpversion(), WP_RIG_MINIMUM_PHP_VERSION, '<' ) ) {
+	require get_template_directory() . '/inc/back-compat.php';
+	return;
+}
+
+// Include WordPress shims.
+require get_template_directory() . '/inc/wordpress-shims.php';
+
+// Setup autoloader (via Composer or custom).
+if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
+	require get_template_directory() . '/vendor/autoload.php';
+} else {
+	/**
+	 * Custom autoloader function for theme classes.
+	 *
+	 * @access private
+	 *
+	 * @param string $class_name Class name to load.
+	 * @return bool True if the class was loaded, false otherwise.
+	 */
+	function _wp_rig_autoload( $class_name ) {
+		$namespace = 'WP_Rig\WP_Rig';
+
+		if ( strpos( $class_name, $namespace . '\\' ) !== 0 ) {
+			return false;
+		}
+
+		$parts = explode( '\\', substr( $class_name, strlen( $namespace . '\\' ) ) );
+
+		$path = get_template_directory() . '/inc';
+		foreach ( $parts as $part ) {
+			$path .= '/' . $part;
+		}
+		$path .= '.php';
+
+		if ( ! file_exists( $path ) ) {
+			return false;
+		}
+
+		require_once $path;
+
+		return true;
+	}
+	spl_autoload_register( '_wp_rig_autoload' );
+}
+
+// Load the `wp_rig()` entry point function.
+require get_template_directory() . '/inc/functions.php';
+
+// Initialize the theme.
+call_user_func( 'WP_Rig\WP_Rig\wp_rig' );
+
+add_action( 'wp_enqueue_scripts', 'pricing_js' );
+function pricing_js() {
+	if(is_page('pricing')){
+		wp_enqueue_script(
+			'pricing-frontend',
+			get_stylesheet_directory_uri() . '/build/pricing.js',
+			['wp-element'],
+			time(), // Change this to null for production
+			true
+		);
+	}else if(is_page('products')){
+		wp_enqueue_script(
+			'pricing-frontend',
+			get_stylesheet_directory_uri() . '/build/products.js',
+			['wp-element'],
+			time(), // Change this to null for production
+			true
+		);
+	}
+}
---
+++ themes/wprig-customtheme/page-products.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content' );
+
+?>
+	<main id="primary" class="site-main">
+		<?php
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content/entry', 'page' );
+		}
+		?>
+		<div id="<?php echo $post->post_name;?>-root">my content</div>
+	</main><!-- #primary -->
+<?php
+get_sidebar();
+get_footer();
